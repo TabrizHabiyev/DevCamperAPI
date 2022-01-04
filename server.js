aee80866bc359267a405689d6f798bcf50afcb04
TabrizHabiyev <tabrizfh@code.edu.az>
2022-01-04 20:25:04 +0400

    Colors In The Console

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors')
 const connectDB = require('./config/db');
+
 
 
 //Load env vars
@@ -27,11 +29,11 @@ app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000
 
-const server = app.listen(PORT,console.log(`server running on${process.env.NODE_ENV} PORT ${PORT}`));
+const server = app.listen(PORT,console.log(`server running on${process.env.NODE_ENV} PORT ${PORT}`.yellow.bold));
 
 // Handle unhandled rejections
 process.on('unhandledRejection',(err,promise)=>{
-   console.log(`Error : ${err.message}`);
+   console.log(`Error : ${err.message}.red`);
    //Close server & exit process
    server.close(()=> process.exit(1))
 });
