a431da97909b6ba5c3ef7cbca7de6bd870beacb4
TabrizHabiyev <tabrizfh@code.edu.az>
2022-01-21 11:06:45 +0400

    Bootcamp user relationship

--- Seeder.js
+++ Seeder.js
@@ -16,6 +16,7 @@ dotenv.config({path: './config/config.env'});
 //Load models
 const Bootcamp = require('./models/Bootcamps');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 //Coonect database
 mongoose.connect(process.env.MONGO_URI,{
@@ -28,6 +29,8 @@ mongoose.connect(process.env.MONGO_URI,{
 
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
+
 
  //Import into DB
 
@@ -35,6 +38,7 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'u
      try {
          await Bootcamp.create(bootcamps);
          await Course.create(courses);
+         await User.create(users);
          console.log('Data imported ....'.green.inverse);
          process.exit();
      } catch (error) {
@@ -48,13 +52,13 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'u
     try {
         await Bootcamp.deleteMany()
         await Course.deleteMany()
+        await User.deleteMany()
         console.log('Data destroyed ....'.red.inverse);
         process.exit();
     } catch (error) {
         console.error(error);
     }
 }
-
 
 if (process.argv[2] === '-i') {
     importData();
--- models/bootcamps.js
+++ models/bootcamps.js
@@ -116,8 +116,15 @@ const BootcampSchema = new mongoose.Schema({
           createdAt:{
               type: Date,
               default:Date.now
-          }
-}, {
+          },
+
+          user:{
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: true
+        }
+}, 
+{
   toJSON:{ virtuals:true },
   toObject:{ virtuals:true}
 });
